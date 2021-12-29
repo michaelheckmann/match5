6c9fc93c0ba10442f741812796e4207651d7b9a0
michael_heckmann <michael.heckmann@qart.shop>
2021-12-29 16:55:06 +0100

    fix db path

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const { client, q } = require("../src/db");
+  const { client, q } = require("../../utilities/db");
   var createP = client.query(
     q.Get(q.Ref(q.Collection("Names"), "319327366253904072"))
   );
