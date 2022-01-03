5db3e20e028ce7f161e4b9251496a8d0cc1f78ea
michael_heckmann <michael.heckmann@qart.shop>
2022-01-04 00:23:16 +0100

    fix build error

--- components/Modal.js
+++ components/Modal.js
@@ -9,18 +9,18 @@ const Modal = React.memo(
 
     return ReactDOM.createPortal(
       <>
-        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+          <div className="relative w-auto max-w-3xl mx-auto my-6">
             {/*content*/}
-            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
               {/*header*/}
-              <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+              <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-blueGray-200">
                 <h3 className="text-3xl font-semibold">{title}</h3>
                 <button
-                  className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none focus:outline-none"
                   onClick={closeModal}
                 >
-                  <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  <span className="block w-6 h-6 text-2xl text-black bg-transparent outline-none focus:outline-none">
                     ✕
                   </span>
                 </button>
@@ -28,9 +28,9 @@ const Modal = React.memo(
               {/*body*/}
               {children}
               {/*footer*/}
-              <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+              <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-blueGray-200">
                 <button
-                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  className="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none background-transparent focus:outline-none"
                   type="button"
                   onClick={buttonFunction}
                 >
@@ -40,11 +40,13 @@ const Modal = React.memo(
             </div>
           </div>
         </div>
-        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
       </>,
       domEl
     );
   }
 );
+
+Modal.displayName = "Modal";
 
 export default Modal;
